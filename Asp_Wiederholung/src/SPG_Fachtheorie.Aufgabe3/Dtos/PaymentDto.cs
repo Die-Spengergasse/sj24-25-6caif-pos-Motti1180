8efbc47e0c6549fc22e3f25eb526b58289c2e7cc
Moritz Schneemann <sch19703@spengergasse.at>
2025-03-05 19:46:32 +0100

    finished

---
+++ Asp_Wiederholung/src/SPG_Fachtheorie.Aufgabe3/Dtos/PaymentDto.cs
@@ -0,0 +1,9 @@
+﻿using SPG_Fachtheorie.Aufgabe1.Model;
+
+namespace SPG_Fachtheorie.Aufgabe3.Dtos
+{
+    public record PaymentDto(
+        int id, string employeeFirstName,
+        string employeeLastName, int cashDeskNumber, 
+        string paymentType, int totalAmount);
+}
